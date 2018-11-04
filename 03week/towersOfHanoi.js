361b7dfcfb4c0f5fab8384c345a3153083c97e3d
Eduardo Garza <eduardogarza43@yahoo.com>
2018-11-04 01:57:57 -0500

    checkpoint 1

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -1,4 +1,24 @@
 'use strict';
+// Move piece from stack to stack 
+// Working is legal function 
+// Working check for win function 
+// Working reset function 
+// Legal Move function 
+// Clean PR & es6 syntax
+// Function plan in comments(each function has a stated purpose and method
+// At least 2 tests
+
+//choose starting stack, choose ending stack
+//pop the top piece of startStack and push into endStack (trim and lower case)
+//isLegal checks is value chosen is smaller than endStack value
+//if YES, allow piece to be moved (movePiece)
+//if NO, then return 'starting stack can't be larger than end value' or 'pick again'
+//if legal, move piece and print it on board
+//check for win (all numbers on stack c)
+//if YES, return 'you win!'
+//if NO, keep playing
+// play until win
+
 
 const assert = require('assert');
 const readline = require('readline');
@@ -19,24 +39,62 @@ function printStacks() {
   console.log("c: " + stacks.c);
 }
 
-function movePiece() {
-  // Your code here
-
+function reset(){
+  stacks = {
+    a: [4, 3, 2, 1],
+    b: [],
+    c: []
+  };
 }
 
-function isLegal() {
-  // Your code here
+function gameOver(){
+  printStacks();
+  reset();
+}
 
+function movePiece(startStack, endStack) {
+  //Your code here
+  //endStack.push(startStack.pop());
+  const pickedNumber = startStack.pop();
+  endStack.push(pickedNumber);
+} 
+
+function isLegal(startStack, endStack) {
+  // Your code here
+  if (startStack[startStack.length - 1] < endStack[endStack.length - 1] || endStack.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function checkForWin() {
   // Your code here
-
+  if (stacks.c.length == 4) {
+    console.log("Victory!");
+    gameOver();
+  }
 }
 
 function towersOfHanoi(startStack, endStack) {
   // Your code here
+  const validOptions = 'abc';
+  const ssVal = startStack.trim().toLowerCase();
+  const esVal = endStack.trim().toLowerCase();
 
+  if (validOptions.search(ssVal)>=0 && validOptions.search(esVal)>=0) {
+    const firstPick = stacks[ssVal];
+    const secondPick = stacks[esVal];
+    if (isLegal(firstPick, secondPick)) {
+      movePiece(firstPick, secondPick);
+      checkForWin();
+    } else {
+      console.log('It Does NOT Work!');
+    }
+  }
+  else  {
+    console.log('Pick a, b or c')
+  }
 }
 
 function getPrompt() {
@@ -84,6 +142,18 @@ if (typeof describe === 'function') {
       assert.equal(checkForWin(), true);
       stacks = { a: [1], b: [4, 3, 2], c: [] };
       assert.equal(checkForWin(), false);
+    });
+  });
+  describe('#towersOfHanoi()', () => {
+    it('should not allow a number', () => {
+      towersOfHanoi('a', '9');
+      assert.deepEqual('Pick a, b or c');
+    });
+  });
+  describe('#gameOver()', () => {
+    it('should reset the board', () => {
+      assert.equal(stacks = { a: [4, 3, 2, 1], b: [4, 3, 2, 1], c: [] });
+
     });
   });
 
